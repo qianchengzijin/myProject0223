9d2badb36d7638a5d754e789cc775ae5a09f46f7
zhangzhe <292546783@qq.com>
2019-02-23 20:50:09 +0800

    Initial commit

---
+++ javaWeb/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/2/23 0023
+  Time: 下午 3:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>尚硅谷</title>
+  </head>
+  <body>
+    <h1 style="color: red">尚硅谷红色标题</h1>
+    <h2 style="color: blue">尚硅谷蓝色标题</h2>
+  </body>
+</html>
---
+++ src/com/zhangzhe/test/Hello.java
@@ -0,0 +1,29 @@
+package com.zhangzhe.test;
+
+import java.util.ArrayList;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("hhh");
+        System.out.println("hell");
+        System.out.println("args = [" + args + "]");
+        System.out.println("Hello.main");
+        System.out.println("args = " + args);
+        int num = 8;
+        System.out.println("num = " + num);
+
+        for (int i = 0; i < 100; i++) {
+
+        }
+
+        ArrayList<String> strings = new ArrayList<>();
+        for (String string : strings) {
+            
+        }
+        for (int aa = 0; aa < 88; aa++) {
+
+        }
+
+    }
+}
