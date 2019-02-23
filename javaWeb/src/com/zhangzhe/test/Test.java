0fe52af07e2248b70dfd1148632dc895b72790a1
zhangzhe <292546783@qq.com>
2019-02-23 20:59:36 +0800

    创建测试类

--- javaWeb/src/com/zhangzhe/test/Test.java
+++ javaWeb/src/com/zhangzhe/test/Test.java
@@ -1,9 +1,23 @@
 package com.zhangzhe.test;
 
+import java.util.ArrayList;
+
 public class Test {
 
 
     public static void main(String[] args) {
 
+        System.out.println("dfdfd");
+        System.out.println("htllo");
+        for (int i = 0; i < 100; i++) {
+
+        }
+
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("001");
+        strList.add("002");
+        for (String str : strList) {
+            System.out.println(str);
+        }
     }
 }
